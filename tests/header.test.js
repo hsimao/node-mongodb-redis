b1dfadc0cd27e59ce6a52f779775348997d3e6d4
hsimao <e087754958@gmail.com>
2020-12-26 14:30:12 +0800

    add: header test google auth login

--- tests/header.test.js
+++ tests/header.test.js
@@ -14,7 +14,36 @@ afterEach(async () => {
   await browser.close();
 });
 
-it('We can launch a browser', async () => {
+it('The header has the correct text', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
   expect(text).toEqual('Blogster');
+});
+
+it('Clicking login starts oauth flow', async () => {
+  await page.click('.right a');
+  const url = await page.url();
+  expect(url).toMatch(/accounts.google.com/);
+});
+
+it.only('When signed in, shows logout button', async () => {
+  const id = '5f84532d9534d2861c982769';
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+  console.log(sessionString, sig);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
 });
