cb11698213f19830f0c3ae6d7d9debc4c7dbea87
hsimao <e087754958@gmail.com>
2021-01-01 13:34:43 +0800

    add: test login can see blog create form

--- tests/header.test.js
+++ tests/header.test.js
@@ -24,8 +24,6 @@ it('Clicking login starts oauth flow', async () => {
 
 it('When signed in, shows logout button', async () => {
   await page.login();
-  // 因為上面刷新頁面按鈕不會馬上出現, 需要透過 waitFor ,等待該元素出現後才接續執行後續邏輯
-  await page.waitFor('a[href="/auth/logout"]');
   const text = await page.getContentsOf('a[href="/auth/logout"]');
 
   expect(text).toEqual('Logout');
--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -28,7 +28,9 @@ class CustomPage {
 
     await this.page.setCookie({ name: 'session', value: session });
     await this.page.setCookie({ name: 'session.sig', value: sig });
-    await this.page.goto('localhost:3000');
+    await this.page.goto('localhost:3000/blogs');
+    // 因為上面刷新頁面按鈕不會馬上出現, 需要透過 waitFor ,等待該元素出現後才接續執行後續邏輯
+    await this.page.waitFor('a[href="/auth/logout"]');
   }
 
   async getContentsOf(selector) {
